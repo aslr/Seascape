12c2a46c3a6732f99b74c89409b44ca323133ffe
João Varela <joaocvarela@gmail.com>
2018-08-09 23:22:44 +0100

    Attempt at using a triple buffer render engine.

--- Seascape/Renderer.swift
+++ Seascape/Renderer.swift
@@ -22,11 +22,17 @@ class Renderer: NSObject, MTKViewDelegate
     
     public let device: MTLDevice
     
-    // private ivars
+    // private ivars for triple buffering
+    private let textureCount = 3
+    private var textureQueue:[MTLTexture]!
+    private var currentTexture:MTLTexture!
+    
+    // other ivars
     private var view:MTKView
     private let commandQueue: MTLCommandQueue
     private var pipelineState: MTLComputePipelineState!
     private var inputBuffer: MTLBuffer!
+    private var semaphore: DispatchSemaphore!
     
     // ---------------------------------------------------------------------------------
     // init
@@ -36,14 +42,18 @@ class Renderer: NSObject, MTKViewDelegate
         self.device = metalKitView.device!
         self.commandQueue = device.makeCommandQueue()!
         self.view = metalKitView
+        self.textureQueue = []
         super.init()
         self.pipelineState = createPipeline()
+        guard let layer = metalKitView.layer as? CAMetalLayer else { fatalError() }
+        createScreenTextures(size: metalKitView.drawableSize, scale: layer.contentsScale)
+        self.semaphore = DispatchSemaphore(value: textureCount)
     }
     
     // ---------------------------------------------------------------------------------
     // createPipeline
     // ---------------------------------------------------------------------------------
-    func createPipeline() -> MTLComputePipelineState?
+    private func createPipeline() -> MTLComputePipelineState?
     {
         let library = device.makeDefaultLibrary()!
         
@@ -67,36 +77,92 @@ class Renderer: NSObject, MTKViewDelegate
     }
     
     // ---------------------------------------------------------------------------------
+    // createScreenTextures
+    // ---------------------------------------------------------------------------------
+    private func createScreenTextures(size:CGSize, scale:CGFloat)
+    {
+        textureQueue.removeAll()
+        currentTexture = nil
+        guard let metalLayer = view.layer as? CAMetalLayer else { fatalError() }
+        let pixelformat = metalLayer.pixelFormat
+        
+        let descriptor = MTLTextureDescriptor.texture2DDescriptor(pixelFormat: pixelformat,
+                                                                  width: Int(size.width),
+                                                                  height: Int(size.height),
+                                                                  mipmapped: false)
+        
+        descriptor.usage = [.shaderRead, .shaderWrite]
+        
+        for i in 0..<textureCount
+        {
+            guard let texture = device.makeTexture(descriptor: descriptor) else { fatalError() }
+            texture.label = "Seascape #\(i)"
+            textureQueue.append(texture)
+        }
+    }
+    
+    // ---------------------------------------------------------------------------------
     // draw
     // ---------------------------------------------------------------------------------
     func draw(in view: MTKView)
     {
+        _ = semaphore.wait(timeout: .distantFuture)
+        time += timeStep
+        let inputBufferPtr = inputBuffer.contents().bindMemory(to: float4.self, capacity: 1)
+        inputBufferPtr.pointee = float4(Float(mouse.x),Float(mouse.y),0.0,time)
+        guard let commandBuffer = commandQueue.makeCommandBuffer() else { return }
+        let blockSemaphore = semaphore
+        commandBuffer.addCompletedHandler({_ in blockSemaphore?.signal()})
+        encodeCompute(commandBuffer)
+        
         if let drawable = view.currentDrawable
         {
-            time += timeStep
-            let inputBufferPtr = inputBuffer.contents().bindMemory(to: float4.self, capacity: 1)
-            inputBufferPtr.pointee = float4(Float(mouse.x),Float(mouse.y),0.0,time)
-            guard let commandBuffer = commandQueue.makeCommandBuffer() else { return }
-            guard let commandEncoder = commandBuffer.makeComputeCommandEncoder() else { return }
-            commandEncoder.setComputePipelineState(pipelineState)
-            commandEncoder.setTexture(drawable.texture, index: 0)
-            commandEncoder.setBuffer(inputBuffer, offset: 0, index: 0)
-            let w = pipelineState.threadExecutionWidth
-            let h = pipelineState.maxTotalThreadsPerThreadgroup / w;
-            let threadGroupCount = MTLSize(width: w, height: h, depth: 1)
-            let threadGroups = MTLSize(width: (drawable.texture.width + w - 1) / w,
-                                       height: (drawable.texture.height + h - 1) / h,
-                                       depth: 1)
-            commandEncoder.dispatchThreadgroups(threadGroups, threadsPerThreadgroup: threadGroupCount)
-            commandEncoder.endEncoding()
             commandBuffer.present(drawable)
             commandBuffer.commit()
         }
     }
     
+    // ---------------------------------------------------------------------------------
+    // encodeCompute
+    // ---------------------------------------------------------------------------------
+    private func encodeCompute(_ commandBuffer:MTLCommandBuffer)
+    {
+        guard let writeTexture = textureQueue.first else { fatalError() }
+        guard let commandEncoder = commandBuffer.makeComputeCommandEncoder() else { return }
+        commandEncoder.setComputePipelineState(pipelineState)
+        commandEncoder.setTexture(currentTexture, index: 0)
+        commandEncoder.setTexture(writeTexture, index: 1)
+        commandEncoder.setBuffer(inputBuffer, offset: 0, index: 0)
+        let w = pipelineState.threadExecutionWidth
+        let h = pipelineState.maxTotalThreadsPerThreadgroup / w;
+        let threadGroupCount = MTLSize(width: w, height: h, depth: 1)
+        let threadGroups = MTLSize(width: (writeTexture.width + w - 1) / w,
+                                   height: (writeTexture.height + h - 1) / h,
+                                   depth: 1)
+        commandEncoder.dispatchThreadgroups(threadGroups, threadsPerThreadgroup: threadGroupCount)
+        commandEncoder.endEncoding()
+        rotateTextures()
+    }
+    
+    // ---------------------------------------------------------------------------------
+    // mtkView(_:drawableSizeWillChange)
+    // ---------------------------------------------------------------------------------
+    // Callback when the user changes the size of the window
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize)
     {
-        // let aspect = Float(size.width) / Float(size.height)
+        guard let scale = view.layer?.contentsScale else { return }
+        createScreenTextures(size: size, scale: scale)
+    }
+    
+    // ---------------------------------------------------------------------------------
+    // rotateTextures                                                         private
+    // ---------------------------------------------------------------------------------
+    // rotate the texture queue
+    private func rotateTextures()
+    {
+        currentTexture = textureQueue.first
+        textureQueue.remove(at: 0)
+        textureQueue.append(currentTexture)
     }
 }
 
